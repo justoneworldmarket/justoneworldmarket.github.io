5032ce76bae8eadd069ade241659a7ee047875c3
Russ Miyagawa <russ@redwise.ca>
2025-11-02 17:41:24 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,58 @@
+// Fireup the plugins
+$(document).ready(function(){
+
+    $('#carousel').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    itemWidth: 210,
+    itemMargin: 5,
+    asNavFor: '#slider',
+    start: function(slider) {
+        slider.removeClass('loading');
+    }
+  });
+    
+	// initialise  slideshow
+	 $('#slider').flexslider({
+        animation: "slide",
+        slideshow: false,
+        controlNav: false,
+        animationLoop: false,
+        sync: "#carousel",
+        start: function(slider) {
+          $('body').removeClass('loading');
+        }
+      });
+
+});
+/**
+ * Handles toggling the navigation menu for small screens.
+ */
+( function() {
+	var button = document.getElementById( 'topnav' ).getElementsByTagName( 'div' )[0],
+	    menu   = document.getElementById( 'topnav' ).getElementsByTagName( 'ul' )[0];
+
+	if ( undefined === button )
+		return false;
+
+	// Hide button if menu is missing or empty.
+	if ( undefined === menu || ! menu.childNodes.length ) {
+		button.style.display = 'none';
+		return false;
+	}
+
+	button.onclick = function() {
+		if ( -1 == menu.className.indexOf( 'srt-menu' ) )
+			menu.className = 'srt-menu';
+
+		if ( -1 != button.className.indexOf( 'toggled-on' ) ) {
+			button.className = button.className.replace( ' toggled-on', '' );
+			menu.className = menu.className.replace( ' toggled-on', '' );
+		} else {
+			button.className += ' toggled-on';
+			menu.className += ' toggled-on';
+		}
+	};
+} )();
